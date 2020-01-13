5cf35164e412f6f3c213bf67366c7472505375e1
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2020-01-13 23:35:45 +0100

    Update webpack.ts

--- src/webpack.ts
+++ src/webpack.ts
@@ -1,11 +1,9 @@
-import * as path from 'path';
 import icur from './index';
 import { loader } from 'webpack';
 
 const icurLoader: loader.Loader = function icurLoader(source) {
-  const locale = path.basename(this.resourcePath, '.json');
   const messages = JSON.parse(String(source));
-  const { code } = icur(messages, { locale });
+  const { code } = icur(messages);
   return code;
 };
 
