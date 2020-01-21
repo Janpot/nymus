d1f168a32bf7b2235b426002d8edb37ae74f8ae6
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2020-01-21 10:08:30 +0100

    refactor example scripts

--- example/scripts/build.js
+++ example/scripts/build.js
@@ -1,6 +1,8 @@
 const concurrently = require('concurrently');
 const fs = require('fs');
 const path = require('path');
+const { promisify } = require('util');
+const fsWriteFile = promisify(fs.writeFile);
 
 const PORT = 3000;
 const LOCALES = ['en', 'nl', 'fr'];
@@ -9,17 +11,32 @@ function getPort(i) {
   return PORT + 1 + i;
 }
 
-const rules = LOCALES.map((locale, i) => ({
-  pathname: `/${locale}`,
-  dest: `http://localhost:${getPort(i)}`
-}));
-fs.writeFileSync(
-  path.resolve(__dirname, '../proxy.json'),
-  JSON.stringify({ rules }, null, 2),
-  { encoding: 'utf-8' }
-);
+async function buildProxyRules() {
+  const rules = LOCALES.map((locale, i) => ({
+    pathname: `/${locale}`,
+    dest: `http://localhost:${getPort(i)}`
+  }));
 
-const commands = LOCALES.map(
-  (locale, i) => `LOCALE=${locale} LOCALES=${LOCALES.join(',')} next build`
-);
-concurrently(commands);
+  await fsWriteFile(
+    path.resolve(__dirname, '../.next/rules.json'),
+    JSON.stringify({ rules }, null, 2),
+    { encoding: 'utf-8' }
+  );
+}
+
+async function buildNext() {
+  const commands = LOCALES.map((locale, i) => ({
+    name: `build:${locale}`,
+    command: `LOCALE=${locale} LOCALES=${LOCALES.join(',')} next build`
+  }));
+  await concurrently(commands);
+}
+
+async function main() {
+  await Promise.all([buildProxyRules(), buildNext()]);
+}
+
+process.on('unhandledRejection', err => {
+  throw err;
+});
+main();
--- example/scripts/start.js
+++ example/scripts/start.js
@@ -7,22 +7,31 @@ function getPort(i) {
   return PORT + 1 + i;
 }
 
-const commands = LOCALES.map((locale, i) => {
-  const port = getPort(i);
-  return {
-    command: `LOCALE=${locale} LOCALES=${LOCALES.join(
-      ','
-    )} next start -p ${port}`,
-    name: `locale:${locale}`
-  };
-});
+async function main() {
+  const commands = LOCALES.map((locale, i) => {
+    const port = getPort(i);
+    const locales = LOCALES.join(',');
+    return {
+      command: `LOCALE=${locale} LOCALES=${locales} next start -p ${port}`,
+      name: `start:${locale}`
+    };
+  });
 
-concurrently(
-  [
-    { command: `npm:proxy -s -- -r ./proxy.json -p ${PORT}`, name: 'proxy' },
-    ...commands
-  ],
-  {
-    killOthers: ['success', 'failure']
-  }
-);
+  await concurrently(
+    [
+      {
+        command: `micro-proxy -r ./.next/rules.json -p ${PORT}`,
+        name: 'proxy'
+      },
+      ...commands
+    ],
+    {
+      killOthers: ['success', 'failure']
+    }
+  );
+}
+
+process.on('unhandledRejection', err => {
+  throw err;
+});
+main();
