08b7bdaebbb0bb331b081dbe1fcea7378d83d323
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2020-01-25 14:11:18 +0100

    cleaup _app

--- docs/pages/_app.tsx
+++ docs/pages/_app.tsx
@@ -1,44 +1,44 @@
-import './_app.css';
+import '../src/app.css';
 import 'codemirror/lib/codemirror.css';
 import 'highlight.js/styles/default.css';
 
 import React from 'react';
-import App /* , { AppProps } */ from 'next/app';
+import { AppProps } from 'next/app';
 import Head from 'next/head';
 import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import theme from '../src/theme';
 import Layout from '../src/components/Layout';
 
-export default class MyApp extends App {
-  componentDidMount() {
+export default function MyApp({ Component, pageProps }: AppProps) {
+  React.useEffect(() => {
     // Remove the server-side injected CSS.
     const jssStyles = document.querySelector('#jss-server-side');
     if (jssStyles) {
       jssStyles.parentElement!.removeChild(jssStyles);
     }
-  }
+  }, []);
 
-  render() {
-    const { Component, pageProps } = this.props;
-
-    return (
-      <React.Fragment>
-        <Head>
-          <title>nymus</title>
-          <meta
-            name="viewport"
-            content="minimum-scale=1, initial-scale=1, width=device-width"
-          />
-        </Head>
-        <ThemeProvider theme={theme}>
-          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-          <CssBaseline />
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </ThemeProvider>
-      </React.Fragment>
-    );
-  }
+  return (
+    <React.Fragment>
+      <Head>
+        <title>nymus</title>
+        <meta
+          name="description"
+          content="Transform ICU messages into React components."
+        />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </React.Fragment>
+  );
 }
